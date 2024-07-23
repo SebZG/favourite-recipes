214ef37e7f26437e8e267ae5d64f3138990b87fb
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-23 20:20:49 +0100

    Add import store @ index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 
+import { store } from './app/store';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+
+const render = () => {
+	root.render(
+		<React.StrictMode>
+			<App state={store.getState()} dispatch={store.dispatch} />
+		</React.StrictMode>
+	);
+}
+
+render();
+
+store.subscribe(render);
