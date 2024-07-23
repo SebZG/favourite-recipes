16fc55c272376974798b0de4cfc9388d8297dbc7
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-23 18:12:31 +0100

    Add images + images data

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const allRecipesData = [
+   { id: 0, name: "Burger", img: "./images/burger.jpeg" },
+   { id: 1, name: "Chicken Wings", img: "./images/chicken-wings.jpeg" },
+   { id: 2, name: "Empanada", img: "./images/empanada.jpeg" },
+   { id: 3, name: "Fish 'n' Chips", img: "./images/fish-and-chips.jpeg" },
+   { id: 4, name: "Pasta", img: "./images/pasta.jpeg" },
+   { id: 5, name: "Pizza", img: "./images/pizza.jpeg" },
+   { id: 6, name: "Sancocho", img: "./images/sancocho.jpeg" },
+   { id: 7, name: "Arepa", img: "./images/arepa.jpg" },
+   { id: 8, name: "Bandeja Paisa", img: "./images/bandeja-paisa.jpg" },
+   { id: 9. name: "Kebab", img: "./images/kebab.jpg" }
+]
+
+export default allRecipesData;
